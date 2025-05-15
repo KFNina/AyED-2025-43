03b3334486ee8f89e614931bd4428ace78df119b
KFNina <ninaquispekevinfernando@gmail.com>
2025-05-15 13:51:45 +0200

    Add files via upload

---
+++ Etapa 0/0-Nina_2/0-Nina_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nina_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            Console.WriteLine("¿Cual es tu nombre?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("¿Cual es tu edad?");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hola " + nombre + " tu edad es " + edad);
+
+            Console.ReadKey();
+        }
+    }
+}
