905d8b8a2cb0b9b8f7d5b4d8bb6bd7c58234dcb4
KFNina <ninaquispekevinfernando@gmail.com>
2025-05-19 16:04:12 +0200

    Add files via upload

---
+++ Etapa 1/1-Nina_14/1-Nina_14/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nina_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Brajan";
+            int salud = 100;
+            int monedas = 50;
+            int enemigosDerrotados = 0;
+            bool tieneEspada = false;
+            int opc;
+
+            do
+            {
+                Console.WriteLine("1. Mostrar estado del jugador");
+                Console.WriteLine("2. Encontrar espada mágica");
+                Console.WriteLine("3. Pelear contra un enemigo");
+                Console.WriteLine("4. Comprar poción de vida(vale 20 monedas)");
+                Console.WriteLine("5. Salir del juego");
+                Console.WriteLine("Seleccione una opción:");
+
+                opc = Convert.ToInt32(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Hola Brajan tu salud es de " + salud);
+                            Console.WriteLine("Tus monedas son " + monedas);
+                            Console.WriteLine("Los enemigos derrotados son " + enemigosDerrotados);
+                            if (tieneEspada == false)
+                                Console.WriteLine("Y tu no tienes la espada");
+                            else
+                                Console.WriteLine("Tu tienes la espada");
+                        }
+                        break;
+                    case 2:
+                        {
+                            if (tieneEspada == false)
+                            {
+                                Console.WriteLine("¡Encontraste la espada mágica!");
+                                tieneEspada = true;
+                            }
+                            else
+                                Console.WriteLine("Ya tenés la espada mágica.");
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            if (tieneEspada == false)
+                            {
+                                Console.WriteLine("¡Fue una pelea dura sin espada! Salud -30.");
+                                salud = salud - 30;
+                            } 
+                            else
+                                Console.WriteLine("¡Ganaste la batalla! Salud -10.");
+                            salud = salud - 10;
+                            enemigosDerrotados = enemigosDerrotados + 1;
+                        }
+                        break;
+                    case 4:
+                        {
+                            if (monedas >= 20)
+                            {
+                                monedas = monedas - 20;
+                                salud = salud + 20;
+                                Console.WriteLine("Usaste una poción. +20 de salud.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No tenés suficientes monedas.");
+                            }
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("¡Gracias por jugar!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+
+
+            }
+            while (opc != 5);
+
+            Console.ReadKey();
+        
+    }
+        }
+}
---
+++ Etapa 1/1-Nina_15/1-Nina_15/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nina_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salud = 100;
+            int hambre = 50;
+            int energia = 70;
+            int dia = 1;
+            bool sigueVivo = true;
+            Random rand = new Random();
+            int opc;
+
+            do
+            {
+                int probabilidad = rand.Next(1, 101);
+                Console.WriteLine("Día " + dia + "– ¿Qué querés hacer?");
+                Console.WriteLine("1. Buscar comida");
+                Console.WriteLine("2. Dormir");
+                Console.WriteLine("3. Explorar la isla");
+                Console.WriteLine("4. Ver estado del personaje");
+                Console.WriteLine("5. Salir del juego");
+                Console.WriteLine("Elija una opción:");
+                opc = Convert.ToInt32(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        {
+                            if (probabilidad <= 30)
+                            {
+                                salud = salud - 15;
+                                Console.WriteLine("Comiste algo en mal estado.Salud - 15.");
+                            }
+                            else
+                            {
+                                hambre = hambre + 20;
+                                energia = energia - 15;
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            energia = energia + 30;
+                            hambre = hambre - 10;
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            energia = energia - 20;
+                            hambre = hambre - 15;
+                            if (probabilidad >= 50)
+                            {
+                                Console.WriteLine("¡Encontraste una planta curativa! Salud + 10.");
+                                salud = salud + 10;
+                            }
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Tu salud es de " + salud);
+                            Console.WriteLine("Tu hambre es de " + hambre);
+                            Console.WriteLine("Tu energia es de " + energia);
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Juego finalizado");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+
+                }
+                if (salud <= 0 || hambre <= 0 || energia <= 0)
+                {
+                    Console.WriteLine("Te desmayaste y no pudiste sobrevivir... Game Over");
+                    sigueVivo = false;
+                }
+                dia = dia + 1;
+            }
+            while (opc != 5);
+
+            Console.ReadKey();
+        }
+    }
+}
