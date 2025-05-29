83600fe77c666c8955b604d73ac4eda8e97b05b4
KFNina <ninaquispekevinfernando@gmail.com>
2025-05-29 14:29:20 +0200

    Add files via upload

---
+++ Etapa2/2-Nina_5/2-Nina_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Nina_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad, Valor;
+
+            Console.WriteLine("Ingrese la cantidad de objetos");
+            cantidad = int.Parse(Console.ReadLine());
+            int[] objetos = new int[cantidad];
+            Console.WriteLine("Ingrese el valor minimo requerido");
+            Valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Ingrese el valor del objeto " + (i + 1));
+                objetos[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (objetos[i] > Valor)
+                {
+                    Console.WriteLine("Posicion " + i + " " + objetos[i]);
+                }
+                
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa2/2-Nina_6/2-Nina_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Nina_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int candidatos, multi;
+
+            Console.WriteLine("Ingrese la cantidad de candidatos a dar el examen");
+            candidatos = int.Parse(Console.ReadLine());
+
+            int[] puntajes = new int[candidatos];
+
+            for (int i = 0; i < candidatos; i++)
+            {
+                Console.WriteLine("Cual es el puntaje del candidato " + (i + 1));
+                puntajes[i] = int.Parse(Console.ReadLine());
+            }
+            int t;
+            for (int i = 0; i < candidatos; i++)
+            {
+                for (int j = 0; j < candidatos - i - 1; j++)
+                {
+                    if (puntajes[j + 1] < puntajes[j])
+                    {
+                        t = puntajes[j + 1];
+                        puntajes[j + 1] = puntajes[j];
+                        puntajes[j] = t;
+                    }
+                }
+            }
+
+            Console.WriteLine("Ingrese un numero entero");
+            multi = int.Parse(Console.ReadLine());
+
+
+            for ( int i = 0;i < candidatos;i++)
+            {
+                Console.WriteLine("El puntajes es " + puntajes[i]);
+                if (puntajes[i] % multi == 0)
+                {
+                    Console.WriteLine("El valor " + puntajes[i] + " Es multiplo de " + multi);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
