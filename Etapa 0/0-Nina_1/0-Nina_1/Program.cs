a667382fbdd8ba45cfee91e491791b84f88de6fc
KFNina <ninaquispekevinfernando@gmail.com>
2025-06-19 14:59:20 +0200

    Add files via upload

---
+++ Etapa 0/0-Nina_1/0-Nina_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nina_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra = "!Hola Mundo!";
+            Console.WriteLine(palabra);
+            Console.ReadKey();
+        }
+    }
+}
