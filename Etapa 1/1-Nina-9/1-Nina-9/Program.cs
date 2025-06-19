e1400c036056e7109f2ae855d9982da59085cecb
KFNina <ninaquispekevinfernando@gmail.com>
2025-06-19 14:57:38 +0200

    Add files via upload

---
+++ Etapa 1/1-Nina-9/1-Nina-9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nina_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Dime un numero positivo:");
+            num = int.Parse(Console.ReadLine());
+            while(num != 0)
+            {
+                num = num - 1;
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
