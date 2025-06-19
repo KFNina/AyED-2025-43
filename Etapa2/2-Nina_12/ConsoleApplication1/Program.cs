d024a24ea63fadc34e7480e032c6a41f0821e40d
KFNina <ninaquispekevinfernando@gmail.com>
2025-06-19 15:04:07 +0200

    Add files via upload

---
+++ Etapa2/2-Nina_12/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int Ta = 4;
+            int[] vec = new int [Ta];
+
+            int[,] Matriz = new int[Ta,Ta];
+            for (int i = 0; i < Ta; i++)
+            {
+                for (int j = 0; j < Ta; j++)
+                {
+                    int num = rand.Next(1, 25);
+                    Matriz[i, j] = num;
+                }
+            }
+            for (int i = 0; i < Ta; i++)
+            {
+                vec[i] = Matriz[i, i];
+                Console.WriteLine(vec[i]);
+                Console.Write(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
