4b837d865e66e3f51ec17ca8dbcf98a205efd3ba
KFNina <ninaquispekevinfernando@gmail.com>
2025-06-19 14:56:09 +0200

    Add files via upload

---
+++ Etapa 1/1-Nina_4/1-Nina_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad, ingresos;
+            Console.WriteLine("¿Que edad tenes?");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("¿Cuantos ingresos tienes mensualmente?");
+            ingresos = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 19 && ingresos <= 100000)
+                Console.WriteLine("Obtuviste la Beca Chaval");
+            else if (edad < 19 && ingresos == 0)
+            {
+                Console.WriteLine("Obtuviste la beca Chaval");
+            }
+            else if (edad == 19 && ingresos <= 50000)
+                Console.WriteLine("Obtuviste la beca Chaval");
+            else
+                Console.WriteLine("No obtuviste la beca Chavalin");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Etapa 1/1-Nina_5/1-Nina_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nina_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comi, refugio, bate;
+            Console.WriteLine("¿Tienes comida?");
+            comi = (Console.ReadLine().ToUpper());
+            Console.WriteLine("¿Tienes un refugio?");
+            refugio = (Console.ReadLine().ToUpper());
+            Console.WriteLine("¿Tienes un bate?");
+            bate = (Console.ReadLine().ToUpper());
+
+            if (comi == "SI" && refugio == "SI" && bate == "SI")
+                Console.WriteLine("_Tu vas a sobrevivir");
+            else
+                Console.WriteLine("Tu no vas a lograr sobrevivir");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
